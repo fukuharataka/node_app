f22cc95cfe4a6ff47fb62fb390a938ce5f5863d4
fukuharataka <chapio0817@gmail.com>
2018-11-29 16:47:54 +0900

    [Add]クエリーパラメータ

--- app.js
+++ app.js
@@ -15,23 +15,19 @@ console.log('Server start!');
 
 //createServerの処理
 function getFromClient(request, response){
-    var url_parts = url.parse(request.url);
+    var url_parts = url.parse(request.url, true);
     switch (url_parts.pathname) {
 
         case '/':
-            var content = ejs.render(index_page, {
+            var content = "これはIndexページです";
+            var query = url_parts.query;
+            if (query.msg != undefined){
+                var query_obj =
+                content += 'あなたは、「' +query.msg+ '」と送りました';
+            }
+            var content = ejs.render(index_page,{
                 title:"Index",
-                content:"これはindexページです",
-            });
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
-            break;
-
-        case '/other':
-            var content = ejs.render(other_page, {
-                title:"Other",
-                content:"これは新しく用意したページです",
+                content:content,
             });
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(content);
