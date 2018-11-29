f00349ce7d96c1361876fd04846863163cfbe455
fukuharataka <chapio0817@gmail.com>
2018-11-29 18:23:06 +0900

    [Add]data to index.ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
@@ -16,22 +17,15 @@ console.log('Server start!');
 //createServerの処理
 function getFromClient(request, response){
     var url_parts = url.parse(request.url, true);
+
     switch (url_parts.pathname) {
 
         case '/':
-            var content = "これはIndexページです";
-            var query = url_parts.query;
-            if (query.msg != undefined){
-                var query_obj =
-                content += 'あなたは、「' +query.msg+ '」と送りました';
-            }
-            var content = ejs.render(index_page,{
-                title:"Index",
-                content:content,
-            });
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
+            response_index(request, response);
+            break;
+        
+        case '/other':
+            response_other(request, response);
             break;
 
         case '/style.css':
@@ -44,5 +38,64 @@ function getFromClient(request, response){
             response.writeHead(200, {'Content-Type': 'text/plain'});
             response.end('no page...');
             break;
+    }
+}
+
+//追加するデータ用変数 
+var data = {
+    'Taro':'09-999-999',
+    'Hanako':'080-888-888',
+    'Sachiko':'070-777-777',
+    'Ichiro':'060-666-666'
+};
+
+//indexのアクセス処理
+function response_index(request, response){
+    var msg = "これはIndexページです"
+    var content = ejs.render(index_page, {
+        title:"Index",
+        content:msg,
+        data:data,
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end();
+}
+
+//otherのアクセス処理
+function response_other(request, response){
+    var msg = "これはotherページです"
+    //POSTアクセス時の処理
+    if (request.method == 'POST'){
+        var body='';
+
+        //データ受信のイベント処理
+        request.on('data', (data) =>{
+            body +=data;
+        });
+
+        //データ受信終了のイベント処理
+        request.on('data', (data)=>{
+            var post_data = qs.parse(body); //データのパース
+            msg += 'あなたは「' +post_data.msg+ '」と書きました';
+            var content = ejs.render(other_page, {
+                title:"Other",
+                content:msg,
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(content);
+            response.end();
+        });
+
+    //GETのアクセス時の処理
+    } else {
+        var msg ="ページがありません"
+        var content = ejs.render(other_page, {
+            title:"Other",
+            content:msg,
+        });
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(content);
+        response.end();
     }
 }
